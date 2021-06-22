e1903d58ce4ba8e3e15dbcaa24d2a27942d966f1
Amit Kumar Samantara <52200916+amitsamantara@users.noreply.github.com>
2021-06-23 03:26:00 +0530

    Add files via upload
    main cpp file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello World!";
+
+
+    for(int i = 0; i < 50; i++ ){
+       cout << "Hello world!\n";
+    }
+
+    return 0;
+}
